e979747a0e22da85211421bf67a5841776e4e767
Pc15 <Pc15@CPU15>
2026-01-30 11:07:12 -0600

    CodPrincipal

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,28 @@
+
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       
+       System.out.print("Bienvenido a la calculadora mas pro:" );
+       System.out.println("Ingresa el primer numero: ");
+       double a = sc.nextDouble();
+       System.out.println("Ingresa el segundo numero: ");
+       double b = sc.nextDouble();
+       System.out.println("Ingresa el tercer numero: ");
+       double c = sc.nextDouble();
+       System.out.println("Ingresa el cuarto numero: ");
+       double d = sc.nextDouble();
+       
+       //Multiplicacion multiplicacion = new Multiplicacion();
+       //Division division = new Division();
+       //System.out.println("El resultado de la multiplicación es: "+multiplicacion.operacion(a,b,c,d));
+       //System.out.println("El resultado de la division es: "+division.operacion(a,b,c,d);
+       
+    }
+    
+}
